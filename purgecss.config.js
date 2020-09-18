2c5165543e940369b9c030362de69b66c45d2f8f
Ryan Burnette <ryan.burnette@gmail.com>
2020-09-18 16:50:07 -0400

    bugfix

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,8 +1,8 @@
 'use strict';
 
 module.exports = {
-  content: ['public/**.html', 'public/**.js'],
-  css: ['public/**.css'],
+  content: ['public/**/*.html', 'public/**/*.js'],
+  css: ['public/**/*.css'],
   output: 'public/',
   keyframes: true,
   whitelist: [],
